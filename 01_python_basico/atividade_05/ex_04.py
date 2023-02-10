d716cc32d95d426df9f35a358ef6aef5eef018c5
Marlisson <marlisson.business@gmail.com>
2023-02-10 01:39:42 +0000

    feat: atividade_09

--- 01_python_basico/atividade_05/ex_04.py
+++ 01_python_basico/atividade_05/ex_04.py
@@ -1,17 +1,14 @@
 """
-**Questão 4:** Faça um programa que faz o seguinte: 
-
-**a.** lê um número inteiro par N fornecido pelo usuário. O programa deve continuar solicitando o número N enquanto o valor entrado não for par;
-
-**b.** preenche uma lista de tamanho N com números inteiros fornecidos pelo usuário;
-
-**c.** faz a “dobra ao meio” do vetor. Ou seja, você irá criar um vetor com a metade do número de elementos do vetor original, 
+Questão 4: Faça um programa que faz o seguinte: 
+a. lê um número inteiro par N fornecido pelo usuário. O programa deve continuar solicitando o número N enquanto o valor entrado não for par;
+b. preenche uma lista de tamanho N com números inteiros fornecidos pelo usuário;
+c. faz a “dobra ao meio” do vetor. Ou seja, você irá criar um vetor com a metade do número de elementos do vetor original, 
 cujas entradas são dadas pela soma dos valores que se encontram quando o vetor é “dobrado ao meio”. Como no exemplo abaixo:
 
-Se o vetor original é dado por 
-v = [4, 5, 8, 9, 3, 7, 6, 1]
-o vetor resultante da dobra ao meio será 
-v’ = [5, 11, 15, 12] (em que, 5=4+1, 11=5+6, 15=8+7 e 12=9+3).
+Se o vetor original é dado por:
+  v = [4, 5, 8, 9, 3, 7, 6, 1]
+O vetor resultante da dobra ao meio será:
+  v’ = [5, 11, 15, 12] (em que, 5=4+1, 11=5+6, 15=8+7 e 12=9+3).
 """
 
 while True:
