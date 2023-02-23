9a1d4870c136d0047a7967e02255e8b0c59f7d4f
Marlisson <marlisson.business@gmail.com>
2023-02-23 20:28:07 +0000

    refact atividade 13

--- Módulo 03 - Python Avançado/aula_13_herança_em_classes/heranca/heranca/main.py
+++ Módulo 03 - Python Avançado/aula_13_herança_em_classes/heranca/heranca/main.py
@@ -6,12 +6,7 @@ Crie também a classe Triangle que herda o Polygon e implementa o “find_area()
 from abc import ABC
 from typing import List
 import math
-
-class TriangleError(Exception):
-    "Raised when a triangle can't be created."
-
-class ZeroOrNegativeValueError(Exception):
-    "Raised when a determined value is zero or negative."
+from triangle_validator import validateTriangle
 
 class Polygon(ABC):
     """ This class was defined as an abstract class because it does not refer to any specific polygon """
@@ -41,46 +36,6 @@ class Triangle(Polygon):
         p = sum([ a, b, c ]) / 2
         Area = math.sqrt(p*(p-a)*(p-b)*(p-c))
         return Area
-
-def validateSides(numSides: int, polygonType: str) -> List[float]:
-    """ This function takes a number of sides and the polygon type name and returns a list of validated side sizes """
-
-    sides = []
-    for sideIterator in range(numSides):
-        print(f'\n{sideIterator + 1}º lado do {polygonType}.')
-        while True:
-            try:
-                side = float(input('\nInforme um número decimal ou inteiro: ').replace(',', '.'))
-                if side <= 0:
-                    raise ZeroOrNegativeValueError
-            except ValueError:
-                print('\nValueError: Os valores informados não podem ser convertidos para números decimais!')
-            except ZeroOrNegativeValueError:
-                print('\nZeroOrNegativeValueError: O valor do tamanho do lado não pode ser igual a zero ou negativo!')
-            else:
-                sides.append(side)
-                break
-    return sides
-
-def validateTriangle(numTriangleSides: int) -> List[float]:
-    """ This function gets the sides of the triangle and returns a list of the validated sides of the triangle """
-
-    while True:
-        try:
-            [ a, b, c ] = validateSides(numTriangleSides, 'triângulo')
-            
-            boolean_1 = a + b <= c
-            boolean_2 = a + c <= b
-            boolean_3 = b + c <= a
-
-            if boolean_1 or boolean_2 or boolean_3:
-                raise TriangleError
-            
-        except TriangleError:
-            print('\nTriangleError: Os lados informados não podem formar um triâgulo!')
-            print('Motivo: O tamanho de um dos lados é igual ou superior à soma dos tamanhos dos outros dois lados.')
-        else:
-            return [ a, b, c ]
 
 def main() -> None:
     """ This is an initialization function that calculates the area of a triangle using classes and methods. """
