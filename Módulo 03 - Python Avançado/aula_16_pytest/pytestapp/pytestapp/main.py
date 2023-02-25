b5d1cbe999da7d044f559fc64bc0e8751b5f4725
Marlisson <marlisson.business@gmail.com>
2023-02-25 02:34:25 +0000

    chore: correção de código

--- Módulo 03 - Python Avançado/aula_16_pytest/pytestapp/pytestapp/main.py
+++ Módulo 03 - Python Avançado/aula_16_pytest/pytestapp/pytestapp/main.py
@@ -1,8 +1,8 @@
-import requests
-import json
+import requests, json
  
 class Advice:
     __slots__ = ['_advice']
+
     def __init__(self) -> None:
         """ Builds a new instance of the Advice class assigning a new advice from a advice api. """
         self._advice = requests.get("https://api.adviceslip.com/advice", {"Accept": "application/json"}).json()['slip']['advice']
@@ -17,7 +17,7 @@ class Advice:
         with open("advices/advices.json", "r") as advice_file:
             content_json = json.loads(advice_file.read())
 
-        content_json.append(json.dumps({"id": len(content_json) + 1, "advice": self._advice}))
+        content_json.append({"id": len(content_json) + 1, "advice": self._advice})
 
         with open("advices/advices.json", "w") as advice_file:
             advice_file.write(json.dumps(content_json))
@@ -26,19 +26,21 @@ class Advice:
     def show_old_advices(cls) -> list:
         """ Returns a list of old advices from json file """
 
-        list_of_dicts = []
         with open("advices/advices.json", "r") as advice_file:
-            content_json = json.loads(advice_file.read())
-
-        for dictionay in content_json:
-            list_of_dicts.append(json.loads(dictionay))
-
-        return list_of_dicts
+            return json.loads(advice_file.read())
 
 def main():
+    """ 
+    Requests a new advice from Advice class and prints it;
+    Writes the advice on a json file; and
+    Gets all recorded advices from the json file and prints it's content.
+    """
+
     advice = Advice()
-    print(advice)
+    print(advice, end="\n\n")
+
     advice.write_advice()
+    
     print(Advice.show_old_advices())
 
 if __name__ == '__main__':
