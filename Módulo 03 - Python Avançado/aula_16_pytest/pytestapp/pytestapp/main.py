34c3e9ffc6a7ab6106171017ccef0004089a5a89
Marlisson <marlisson.business@gmail.com>
2023-02-25 02:10:32 +0000

    feat: troca de arquivo txt por arquivo json

--- Módulo 03 - Python Avançado/aula_16_pytest/pytestapp/pytestapp/main.py
+++ Módulo 03 - Python Avançado/aula_16_pytest/pytestapp/pytestapp/main.py
@@ -1,29 +1,45 @@
 import requests
-import os
-import datetime
+import json
  
 class Advice:
     __slots__ = ['_advice']
     def __init__(self) -> None:
+        """ Builds a new instance of the Advice class assigning a new advice from a advice api. """
         self._advice = requests.get("https://api.adviceslip.com/advice", {"Accept": "application/json"}).json()['slip']['advice']
     
     def __str__(self) -> str:
+        """ Returns the _advice attribute. """
         return self._advice
     
     def write_advice(self) -> None:
-        with open("advices/advices.txt", "a") as advice_file:
-            advice_file.write(f"\n\n{datetime.datetime.now()} \n")
-            advice_file.write(self._advice)
-    
-    def show_old_advices(self) -> str:
-        with open("advices/advices.txt", "r+") as advice_file:
-            return advice_file.read()
+        """ Gets data from a json file, appends a new content to it and rewrite the json file with the modified data. """
+
+        with open("advices/advices.json", "r") as advice_file:
+            content_json = json.loads(advice_file.read())
+
+        content_json.append(json.dumps({"id": len(content_json) + 1, "advice": self._advice}))
+
+        with open("advices/advices.json", "w") as advice_file:
+            advice_file.write(json.dumps(content_json))
+
+    @classmethod
+    def show_old_advices(cls) -> list:
+        """ Returns a list of old advices from json file """
+
+        list_of_dicts = []
+        with open("advices/advices.json", "r") as advice_file:
+            content_json = json.loads(advice_file.read())
+
+        for dictionay in content_json:
+            list_of_dicts.append(json.loads(dictionay))
+
+        return list_of_dicts
 
 def main():
     advice = Advice()
     print(advice)
     advice.write_advice()
-    print(advice.show_old_advices())
+    print(Advice.show_old_advices())
 
 if __name__ == '__main__':
     main()
