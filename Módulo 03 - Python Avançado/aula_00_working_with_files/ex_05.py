1016db1f0e7ce5de365eb0bb172ce1bfc682e5c4
Marlisson <marlisson.business@gmail.com>
2023-02-26 04:08:05 +0000

    add exemplos e comandos

--- Módulo 03 - Python Avançado/aula_00_working_with_files/ex_05.py
+++ Módulo 03 - Python Avançado/aula_00_working_with_files/ex_05.py
@@ -4,12 +4,12 @@
 L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
  
 # Creating a file
-with open("ex_04.txt", "w") as file1:
+with open("ex_05.txt", "a") as file1:
     # Writing data to a file
     file1.write("Hello \n")
     file1.writelines(L)
     file1.close()  # to change file access modes
  
-with open("ex_04.txt", "r+") as file1:
+with open("ex_05.txt", "r+") as file1:
     # Reading from a file
     print(file1.read())
