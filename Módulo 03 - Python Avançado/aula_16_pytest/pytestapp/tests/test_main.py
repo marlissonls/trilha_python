d2a7f54bbe8e78c97b108f0140311445a62fd8cb
Marlisson <marlisson.business@gmail.com>
2023-02-26 14:52:37 +0000

    feat: adição da classe TestAdvice

--- Módulo 03 - Python Avançado/aula_16_pytest/pytestapp/tests/test_main.py
+++ Módulo 03 - Python Avançado/aula_16_pytest/pytestapp/tests/test_main.py
@@ -1,18 +1,19 @@
 from pytestapp.main import Advice
 # import pytest
 
-def test_init():
-    advice = Advice()
-    assert isinstance(advice, Advice)
+class TestAdvice:
+    def test_init(self):
+        advice = Advice()
+        assert isinstance(advice, Advice)
 
-def test_write_advice():
-    advice = Advice()
-    assert advice.write_advice() == "the advice was written to the file succesfully!"
+    def test_write_advice(self):
+        advice = Advice()
+        assert advice.write_advice() == "the advice was written to the file succesfully!"
 
-#@pytest.mark.skip
-def test_write_advice_exception():
-    advice = Advice()
-    assert advice.write_advice() == "Something went wrong!"
+    #@pytest.mark.skip
+    def test_write_advice_exception(self):
+        advice = Advice()
+        assert advice.write_advice() == "Something went wrong!"
 
-def test_show_old_advices():
-    assert type(Advice.show_old_advices()) == list
+    def test_show_old_advices(self):
+        assert type(Advice.show_old_advices()) == list
