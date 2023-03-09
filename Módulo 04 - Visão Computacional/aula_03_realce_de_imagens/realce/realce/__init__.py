d333b314ced74d1c1b62dac10be01ec6af5e2580
Marlisson Silva <marlisson.business@gmail.com>
2023-03-09 20:48:56 -0300

    add atividade 03

--- Módulo 04 - Visão Computacional/aula_03_realce_de_imagens/realce/realce/__init__.py
+++ Módulo 04 - Visão Computacional/aula_03_realce_de_imagens/realce/realce/__init__.py
@@ -3,7 +3,7 @@ import enhance
 __all__ = ['enhance']
 
 if __name__ == '__main__':
-    image_base_path = './img/elaineruido.png'
+    image_base_path: str = './img/elaineruido.png'
     select_gaussian_filter: list = [3, 5, 7, 9, 11]
     new_enhanced_img_path: str = enhance.new_image_enhancement(image_base_path, select_gaussian_filter[0])
     another_enhanced_img_path: str = enhance.new_image_enhancement(new_enhanced_img_path, select_gaussian_filter[0])
--- Módulo 04 - Visão Computacional/aula_03_realce_de_imagens/realce/realce/enhance.py
+++ Módulo 04 - Visão Computacional/aula_03_realce_de_imagens/realce/realce/enhance.py
@@ -81,7 +81,7 @@ def new_image_enhancement(path: str, gaussian_filter: int) -> str:
 
 # see the __init__.py file with this entry point ./__init__.py
 """ if __name__ == '__main__':
-    image_base_path = './img/elaineruido.png'
+    image_base_path: str = './img/elaineruido.png'
     select_gaussian_filter: list = [3, 5, 7, 9, 11]
     new_enhanced_img_path: str = new_image_enhancement(image_base_path, select_gaussian_filter[0])
     another_enhanced_img_path: str = new_image_enhancement(new_enhanced_img_path, select_gaussian_filter[0]) """
