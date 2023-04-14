281ff946cd981350145f775ad5a1cb73edf0b96d
Marlisson Silva <marlisson.business@gmail.com>
2023-04-14 16:49:11 -0300

    fix: tipagem

--- M6 - python_database/atv_02_postgres_db/pypostgres/repository/db_queries.py
+++ M6 - python_database/atv_02_postgres_db/pypostgres/repository/db_queries.py
@@ -1,7 +1,7 @@
 from repository.db_operations import DbOperations
 
 class DbQueries:
-    def __init__(self, _db_operations):
+    def __init__(self, _db_operations: DbOperations):
         self.db_operations: DbOperations = _db_operations
 
     def create_table(self, db_cursor, table):
