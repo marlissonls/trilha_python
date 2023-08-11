99a6c1e848f464985ec2cfc4e8d6dec316d79083
Marlisson Silva <marlisson.business@gmail.com>
2023-08-11 01:09:08 -0300

    add: more samples

---
+++ Módulo 09 - Fast API/class_08/samples/s11_status.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/items/", status_code=201)
+async def create_item(name: str):
+    return {"name": name}
+
+#############################
+
+from fastapi import FastAPI, status
+
+app = FastAPI()
+
+
+@app.post("/items/", status_code=status.HTTP_201_CREATED)
+async def create_item(name: str):
+    return {"name": name}
