dc254989174cd652a1445c27eb60d235479fc7c8
Marlisson Silva <marlisson.business@gmail.com>
2023-08-11 14:30:13 -0300

    fix: secret hash

--- Módulo 09 - Fast API/class_08/helpers/__init__.py
+++ Módulo 09 - Fast API/class_08/helpers/__init__.py
@@ -1,19 +1,22 @@
 from base_models import UserIn
+from os import getenv
+from dotenv import load_dotenv
 from typing import Any
 
+load_dotenv()
 
 def hash_encoder(users: list[UserIn]) -> list[dict[str, Any]]:
 
     users_serializable = [user.model_dump() for user in users]
 
     for user in users_serializable:
-        user['password'] = 'HASH_' + user['password']
+        user['password'] = getenv('SECRET') + user['password']
     
     return users_serializable
 
 
 def hash_decoder(users: list[dict[str, Any]]) -> list[UserIn]:
-    
+
     for user in users:
         user['password'] = user['password'][5:]
 
