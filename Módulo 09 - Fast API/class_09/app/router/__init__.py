a595943d7c4285e1b60d7e364ac665d00fa5ef01
Marlisson Silva <marlisson.business@gmail.com>
2023-08-17 17:48:05 -0300

    add router codes

--- Módulo 09 - Fast API/class_09/app/router/__init__.py
+++ Módulo 09 - Fast API/class_09/app/router/__init__.py
@@ -1,14 +1,18 @@
 from app.base_models import UserIn, UserOut, UserId
 from app.controller import UserController
-from fastapi import FastAPI, status
+from fastapi import APIRouter, status
 from typing import Any
 
-api = FastAPI()
+router = APIRouter(
+    prefix="/user",
+    tags=["user"]
+)
+
 
 controller = UserController()
 
 
-@api.post('/user/', status_code=status.HTTP_201_CREATED, response_model=UserId)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=UserId)
 def create_user(user: UserIn) -> Any:
 
     result = controller.create_user_controller(user)
@@ -16,7 +20,7 @@ def create_user(user: UserIn) -> Any:
     return result
 
 
-@api.get('/user/{user_id}', status_code=status.HTTP_200_OK, response_model=UserOut)
+@router.get('/{user_id}', status_code=status.HTTP_200_OK, response_model=UserOut)
 def get_users(user_id: str) -> Any:
 
     result = controller.get_user_controller(user_id)
@@ -24,7 +28,7 @@ def get_users(user_id: str) -> Any:
     return result
 
 
-@api.get('/users/', status_code=status.HTTP_200_OK, response_model=list[UserOut])
+@router.get('/', status_code=status.HTTP_200_OK, response_model=list[UserOut])
 def get_users() -> Any:
 
     result = controller.get_users_controller()
@@ -32,7 +36,7 @@ def get_users() -> Any:
     return result
 
 
-@api.put('/user/{user_id}', status_code=status.HTTP_200_OK, response_model=UserOut)
+@router.put('/{user_id}', tags=['custom'], status_code=status.HTTP_200_OK, response_model=UserOut)
 def update_user(user_id: str, user: UserIn) -> Any:
 
     result = controller.update_user_controller(user_id, user)
@@ -40,7 +44,7 @@ def update_user(user_id: str, user: UserIn) -> Any:
     return result
 
 
-@api.delete('/user/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_users(user_id: str) -> None:
 
     controller.delete_user_controller(user_id)
