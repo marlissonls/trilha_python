caf9ed206b908874e9796719600f5db11ca2e8a1
Marlisson Silva <marlisson.business@gmail.com>
2023-09-02 17:50:00 -0300

    fix routes returns

--- Módulo 09 - Fast API/class_12/app/router/user_routes.py
+++ Módulo 09 - Fast API/class_12/app/router/user_routes.py
@@ -16,44 +16,29 @@ router = APIRouter(prefix="/user", tags=["user"])
 
 @router.get('/{user_id}', status_code=status.HTTP_200_OK, response_model=UserOut)
 def get_user_by_id(user_id: str) -> Any:
-
-    result = controller.get_user_by_id_controller(user_id)
-
-    return result
+    return controller.get_user_by_id_controller(user_id)
 
 
 @router.get('/', status_code=status.HTTP_200_OK, response_model=list[UserOut])
 def get_users() -> Any:
-
-    result = controller.get_users_controller()
-
-    return result
+    return controller.get_users_controller()
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=UserId)
 def create_user(user: UserIn) -> Any:
-
-    result = controller.create_user_controller(user)
-
-    return result
+    return controller.create_user_controller(user)
 
 
 @router.post('/checkuser', status_code=status.HTTP_200_OK, response_model=UserOut)
 def check_user(form: UserForm) -> Any:
-    result = controller.check_user_controller(form)
-
-    return result
+    return controller.check_user_controller(form)
 
 
 @router.put('/{user_id}', tags=['custom'], status_code=status.HTTP_200_OK, response_model=UserOut)
 def update_user(user_id: str, user: UserIn) -> Any:
-
-    result = controller.update_user_controller(user_id, user)
-
-    return result
+    return controller.update_user_controller(user_id, user)
 
 
 @router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(user_id: str) -> None:
-
     controller.delete_user_controller(user_id)
